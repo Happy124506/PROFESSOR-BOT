7dde80cf7f4a7e6a2dca580d1f691ee6c07e3bda
Happy124506 <128205086+Happy124506@users.noreply.github.com>
2024-02-08 15:07:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://lofiversion7:fRujq99j363rNpNJ@cluster0.2ohc7fl.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vivekrawatlove89:2JqjRcqH9fhS0NAB@cluster0.9ejkrvu.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "False")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
